a34110f4ec611c0e0603e7bf52539df7486b702b
Felix Soest <soestfelix@gmail.com>
2023-07-13 00:02:24 +0200

    mover

--- .github/workflows/mover.py
+++ .github/workflows/mover.py
@@ -2,6 +2,7 @@ import json
 
 with open('.github/workflows/mover.json', 'r') as file:
     data = json.load(file)
+print(data) 
 
 for output, inputs in data.items():
     with open(output, 'w') as file:
